2df95309d1ede6caa1a9bcb3d9d410d5c8dfbf8b
Yoke-Sa <101482957+Yoke-Sa@users.noreply.github.com>
2023-01-11 09:25:25 +0900

    Add files via upload

--- pages/CarMenu.tsx
+++ pages/CarMenu.tsx
@@ -23,17 +23,17 @@ const CarMenu: NextPage = () => {
 	const { userId, setUserId } = useContext(UserIdContext);
 	const PostData: Request = { userId: userId };
 
-	useEffect(() => {
-		if (userId === '') {
-			modal.setModalHander(() => router.push('/'));
-			modal.setContent(
-				<>
-					<h1>チートは辞めてください</h1>
-				</>
-			);
-			modal.open();
-		}
-	}, []);
+	// useEffect(() => {
+	// 	if (userId === '') {
+	// 		modal.setModalHander(() => router.push('/'));
+	// 		modal.setContent(
+	// 			<>
+	// 				<h1>チートは辞めてください</h1>
+	// 			</>
+	// 		);
+	// 		modal.open();
+	// 	}
+	// }, []);
 
 	const autoEnd = async (e: BeforeUnloadEvent) => (e.returnValue = '');
 	useEffect(() => {
@@ -122,30 +122,30 @@ const CarMenu: NextPage = () => {
 			<header>
 				<h1>メニュー</h1>
 				<_BaseButton
-					onClick={onClickEndPage}
-					// onClick={goEnd}
+					// onClick={onClickEndPage}
+					onClick={goEnd}
 					_class="button exit-btn">
 					終わり
 				</_BaseButton>
 			</header>
 
 			<div className="car-menu">
-				<section onClick={onClickDesitination}>
-					{/* <section onClick={goDest}> */}
+				{/* <section onClick={onClickDesitination}> */}
+				<section onClick={goDest}>
 					<h2>新規ルート作成</h2>
 					<p>地図上で任意の目的地を指定することができます。</p>
 					<div className="img1" />
 				</section>
 
-				<section onClick={onClickExistsRoute}>
-					{/* <section onClick={goExist}> */}
+				{/* <section onClick={onClickExistsRoute}> */}
+				<section onClick={goExist}>
 					<h2>既存ルート選択</h2>
 					<p>既に定められた経路を選択して移動することができます。</p>
 					<div className="img2" />
 				</section>
 
-				<section onClick={onClickCarWatch}>
-					{/* <section onClick={goWatch}> */}
+				{/* <section onClick={onClickCarWatch}> */}
+				<section onClick={goWatch}>
 					<h2>車の状況確認</h2>
 					<p>通信中の車体が現在どこにいるかを確認できます。</p>
 					<div className="img3" />
